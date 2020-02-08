7b4649985a4cadefc427c1bee70d870052b96a8b
ilssy <ilze.kapeniece@gmail.com>
2020-02-08 13:56:50 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/03/Derpina-Meme-PNG.png"
+        alt="Derpina"
+      />
+      <p>Meme Gen 9000</p>
+    </header>
+  )
+}
+
+export default Header
